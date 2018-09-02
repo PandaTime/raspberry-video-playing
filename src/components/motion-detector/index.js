677f223d8662d0263209d1182d3c8d8781fd4aa9
Stasiuk Yaroslav <mewmewpizza@Stasiuks-MacBook-Pro.local>
2018-09-02 12:24:06 +0300

    Added listening on dynamical update of the MAX_GYRO_DELTA

--- src/components/motion-detector/index.js
+++ src/components/motion-detector/index.js
@@ -1,14 +1,17 @@
+const fs = require('fs');
 const appRoot = require('app-root-path');
 const logger = require(`${appRoot}/utils/logger`)('motion-detector/index');
 const raspberryController = require('./controllers/raspberry');
 const { ACCELEROMETER } = require(`${appRoot}/config/configuration.json`);
 
-const maxGyroDelta = ACCELEROMETER.MAX_GYRO_DELTA;
+let maxGyroDelta = ACCELEROMETER.MAX_GYRO_DELTA;
 let previousAccelerometerData;
 let activeAccelerometers = 0;
 let previouslyActiveAccelerometers = 0;
 
 raspberryController.updateCb(onAccelerometerData);
+
+watchConfigurationFileChange();
 
 /**
  * @param {Array<Object>} accelerometers - for more info see https://github.com/emersion/node-i2c-mpu6050
@@ -52,6 +55,35 @@ function onActiveAccelerometersChange(cb) {
     }
   }, 500);
 }
+
+
+/**
+ * Watching config file change to dynamically update max delta values to check whether accelerometers changed
+ */
+function watchConfigurationFileChange() {
+  fs.watchFile(`${appRoot}/config/configuration.json`, { encoding: 'buffer' }, (eventType, filename) => {
+    if (!filename) return;
+    fs.readFile(`${appRoot}/config/configuration.json`, (err, data) => {
+      if (err) {
+        logger.error('Could not read config file:', err);
+        return;
+      }
+      try {
+        const newGyroDelta = JSON.parse(data.toString()).ACCELEROMETER.MAX_GYRO_DELTA;
+        if (!isNaN(newGyroDelta)) {
+          logger.info(`Updating delta accelerometer: ${maxGyroDelta} -> ${newGyroDelta}`);
+          maxGyroDelta = newGyroDelta;
+        } else {
+          logger.error('Wasnt able to update gyro delta: not a number.', typeof newGyroDelta, newGyroDelta);
+        }
+      } catch (err) {
+        logger.error('Wasnt able to update gyro delta:', err);
+        return;
+      }
+    });
+  });
+}
+
 
 module.exports = {
   onActiveAccelerometersChange,
