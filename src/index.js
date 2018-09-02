4df00a5d60ef41d507c221f8cda61551b12dd73f
Stasiuk Yaroslav <mewmewpizza@Stasiuks-MacBook-Pro.local>
2018-09-02 05:26:53 +0300

    Added convertions from number of active accelerometers to state name

--- src/index.js
+++ src/index.js
@@ -5,11 +5,42 @@ const logger = require(`${appRoot}/utils/logger`)('components/index');
 const motionDetector = require('./components/motion-detector');
 
 /**
+ * @param {Number} numberOfActiveAccelerometers
+ * @return {String} state
+ */
+function convertToState(numberOfActiveAccelerometers) {
+  let state;
+  switch (numberOfActiveAccelerometers) {
+  case 1:
+    state = 'STATE_1';
+    break;
+  case 2:
+    state = 'STATE_2';
+    break;
+  case 3:
+    state = 'STATE_3';
+    break;
+  case 4:
+    state = 'STATE_4';
+    break;
+  case 5:
+    state = 'STATE_5';
+    break;
+  default:
+    state = 'DEFAULT';
+    break;
+  }
+  return state;
+}
+
+/**
  */
 function init() {
   logger.info('Initializing..');
   motionDetector.onActiveAccelerometersChange((numberOfActiveAccelerometers) => {
     logger.info('Number of active accelerometers changed to:', numberOfActiveAccelerometers);
+    const state = convertToState(numberOfActiveAccelerometers);
+    //playback.updateState(state);
   });
   // motionDetector.listenAccelerometerUpdates((data) => {
   //   logger.debug('Motion data:', data);
