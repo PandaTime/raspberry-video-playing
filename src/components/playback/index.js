d96315c63edb3e7c6d921cb9b662b528ae612379
Stasiuk Yaroslav <mewmewpizza@Stasiuks-MacBook-Pro.local>
2018-09-05 18:53:48 +0300

    Updated for debug(only video sound works)

--- src/components/playback/index.js
+++ src/components/playback/index.js
@@ -14,12 +14,12 @@ let sound;
 /** */
 function init() {
   miioController.connect(MIIO.HOST, MIIO.TOKEN);
-  video = omxController.openVideoFile({
-    filePath: FILE_PATHS.VIDEO_FILE,
-    autoRestart: true,
-    name: 'Video Video',
-  });
-  logger.info('Initialized Video:', video.id);
+  // video = omxController.openVideoFile({
+  //   filePath: FILE_PATHS.VIDEO_FILE,
+  //   autoRestart: true,
+  //   name: 'Video Video',
+  // });
+  // logger.info('Initialized Video:', video.id);
 
   videoSound = omxController.openSoundFile({
     filePath: FILE_PATHS.VIDEO_SOUND_FILE,
@@ -30,20 +30,20 @@ function init() {
   logger.info('Initialized Video Sound:', videoSound.id);
 
   // In fact we need only this cb listener
-  sound = omxController.openSoundFile({
-    filePath: FILE_PATHS.SOUND_FILE,
-    autoRestart: false,
-    layer: 0,
-    name: 'Sound Sound',
-  });
-  sound.setUpdatesListener((data) => {
-    logger.debug(`layer is at ${data.position} / ${data.duration}; currently ${data.status}`);
-    if (!isStatusChangeable && data.position > STATES[currentState].SOUND.SOUND_END_TIME) {
-      updateStatus(true);
-      updateState(lastState);
-    }
-  });
-  logger.info('Initialized Sound:', sound.id);
+  // sound = omxController.openSoundFile({
+  //   filePath: FILE_PATHS.SOUND_FILE,
+  //   autoRestart: false,
+  //   layer: 1,
+  //   name: 'Sound Sound',
+  // });
+  // sound.setUpdatesListener((data) => {
+  //   logger.debug(`layer is at ${data.position} / ${data.duration}; currently ${data.status}`);
+  //   if (!isStatusChangeable && data.position > STATES[currentState].SOUND.SOUND_END_TIME) {
+  //     updateStatus(true);
+  //     updateState(lastState);
+  //   }
+  // });
+  // logger.info('Initialized Sound:', sound.id);
 }
 
 /** */
@@ -51,19 +51,19 @@ function updateOmxPlayer() {
   const currentStateConfig = STATES[currentState];
 
   if (currentStateConfig) {
-    video.setPlayFrames({
-      start: currentStateConfig.VIDEO.VIDEO_START_TIME,
-      end: currentStateConfig.VIDEO.VIDEO_END_TIME,
-    });
+    // video.setPlayFrames({
+    //   start: currentStateConfig.VIDEO.VIDEO_START_TIME,
+    //   end: currentStateConfig.VIDEO.VIDEO_END_TIME,
+    // });
     videoSound.setPlayFrames({
       start: currentStateConfig.VIDEO.AUDIO_START_TIME,
       end: currentStateConfig.VIDEO.AUDIO_END_TIME,
     });
-    sound.setPlayFrames({
-      start: currentStateConfig.SOUND.SOUND_START_TIME,
-      end: currentStateConfig.SOUND.SOUND_END_TIME,
-      shouldPlay: currentStateConfig.SOUND.SHOUND_PLAY,
-    });
+    // sound.setPlayFrames({
+    //   start: currentStateConfig.SOUND.SOUND_START_TIME,
+    //   end: currentStateConfig.SOUND.SOUND_END_TIME,
+    //   shouldPlay: currentStateConfig.SOUND.SHOUND_PLAY,
+    // });
   }
 }
 
@@ -89,7 +89,7 @@ function updateState(newState) {
   logger.info('Updating state to:', newState);
   currentState = newState;
 
-  updateStatus(currentState === DEFAULT_STATE);
+  // updateStatus(currentState === DEFAULT_STATE);
 
   updateOmxPlayer();
   if (stateConf.POWER_SOCKET) {
